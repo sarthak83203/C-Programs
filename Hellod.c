22cc7629f3af5ed9125a1111474f8ba9e97db7a3
sarthak <sarthak.shinde2006@gmail.com>
2024-12-13 21:49:13 +0530

    Added two more Programs

---
+++ Hellod.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World C language");
+
+    return 0;
+}
---
+++ Subtration.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter a First Number: ");
+    scanf("%d",&a);
+    printf("Enter a Second Number: ");
+    scanf("%d",&b);
+    printf("Subtration of %d and %d is %d.",a,b,a-b);
+    return 0;
+
+
+
+}
