118eb3f23c788f365ffad3ba4d41c3691119daaf
sarthak <sarthak.shinde2006@gmail.com>
2025-01-16 16:51:14 +0530

    Even row are reverse and odd as it is

--- 2darraymat.c
+++ 2darraymat.c
@@ -6,15 +6,20 @@ int main(){
     int arr[m][n];
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
-            scanf("%d",arr[i][j]);
+            scanf("%d",&arr[i][j]);
         }
     }
-    for(int k=0;k<m;k++){
-        if(k%2==0){
-            printf("%d",arr[k]);
-        }else{
-            printf("No");
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(i%2==0){
+                printf("%d ",arr[i][j]);
+            }else{
+                printf("%d ",arr[i][n-j-1]);
+            }
         }
+         printf("\n");
     }
+   
+    
 
 }
