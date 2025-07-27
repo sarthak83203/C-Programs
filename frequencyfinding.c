764e0ab3e0f0c38495a446cbb1f346330348179e
sarthak <sarthak.shinde2006@gmail.com>
2025-07-27 10:51:21 +0530

    Finding frequency

--- frequencyfinding.c
+++ frequencyfinding.c
@@ -13,16 +13,18 @@ int main(){
 
     for(int i = 0; i < n; i++){
         int count = 0;
+    
 
+         //This commented part is optional
         // Skip already checked elements
-        int alreadyChecked = 0;
-        for(int k = 0; k < i; k++){
-            if(arr[k] == arr[i]){
-                alreadyChecked = 1;
-                break;
-            }
-        }
-        if(alreadyChecked) continue;
+        // int alreadyChecked = 0;
+        // for(int k = 0; k < i; k++){
+        //     if(arr[k] == arr[i]){
+        //         alreadyChecked = 1;
+        //         break;
+        //     }
+        // }
+        // if(alreadyChecked) continue;
 
         // Count full frequency
         for(int j = 0; j < n; j++){
